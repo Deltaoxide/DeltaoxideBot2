8bb8d734936c20422ec2a1be9753e5e1332536ad
Deltaoxide <67400695+Deltaoxide@users.noreply.github.com>
2020-10-09 20:50:29 +0300

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,14 +1,14 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
-TOKEN = "oauth:ga4t0qc2rouflck9yinzvnlwioy0b2"
+TOKEN = "oauth:*********************"
 NICKNAME = "deltaoxidebot"
 CHANNEL = "deltaoxide"
 LANGUAGE= "tr"
-AppAuth = 'avsa0e6z9a2av5wwlhi4lfbtihdicn'
-BotAuth = '4uxjcmmpbnx6e3nn5s4q7r8jwwcwj5'
-ClientID = '1bi1wsk9ahabzvm9g4o5tas80ghiuo'
+AppAuth = '**************'
+BotAuth = '******************'
+ClientID = '***********************'
 AppHeaders = {'Client-ID':ClientID,'Authorization' : 'Bearer ' + AppAuth}
 BaseURL = 'https://api.twitch.tv/helix/'
 BotID = 547400986
-SpoAppToken = 'YmI1MzYyOTVmOWNiNDg1YzlmYjRmNjk4Y2JiNGZlMDk6OGYxNTVlMDY1MTVhNDRmN2FhMzAwNDdhZTUxOWRmZGY='
+SpoAppToken = '**************************='
 GMT = 3
